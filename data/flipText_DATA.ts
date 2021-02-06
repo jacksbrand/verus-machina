8c432d80ac16ffb45d39600c56abee14fbd39789
Jack <jack@pop-os.localdomain>
2021-02-06 11:05:15 +0000

    begining flip logic

---
+++ data/flipText_DATA.ts
@@ -0,0 +1,8 @@
+const flipText: string[] = [
+  'S E C U R E',
+  'P O W E R F U L',
+  'I N T E G R A T E D',
+  'S C A L E A B L E',
+];
+
+export default flipText;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,45 @@
+import { useState, useEffect } from 'react';
+
+import flipText from '../data/flipText_DATA';
+
+import styles from '../styles/index.module.css';
+
 const IndexPage = () => {
-	return (
-		<div>
-			<h1>Hi There</h1>
-		</div>
-	);
+  const [flip1, setFlip1] = useState('');
+  const [flip2, setFlip2] = useState('');
+  const [flip3, setFlip3] = useState('');
+
+  const fetchRandomFlipText = () => {
+    let textToShow = flipText[Math.floor(Math.random() * flipText.length)];
+    setFlip1(textToShow);
+  };
+
+  useEffect(() => {
+    const timeout = setInterval(() => {
+      fetchRandomFlipText();
+    }, 1000);
+
+    return function cleanup() {
+      clearInterval(timeout);
+    };
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <div>
+        <h1>Verus Machina</h1>
+      </div>
+      <div className={styles.flipcontainer}>
+        <p>{flip1}</p>
+        <p>{flip2}</p>
+        <p>apples</p>
+      </div>
+      <div>
+        <h2>WEB &</h2>
+        <h2>SYSTEMS</h2>
+        <h3>Development</h3>
+      </div>
+    </div>
+  );
 };
 export default IndexPage;
